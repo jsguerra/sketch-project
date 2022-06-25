57ad900b9cff26f7357b7493bc49ed3fcd2c3a52
jsguerra <jose@gcostudios.com>
2022-06-25 14:58:40 +0100

    file updated

--- sketch-02a.js
+++ sketch-02a.js
@@ -4,6 +4,11 @@ const settings = {
   dimensions: [1080, 1080],
 };
 
+// Function to convert degrees into radians
+const degToRad = (degrees) => {
+  return (degrees / 180) * Math.PI;
+};
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = "white";
@@ -11,19 +16,36 @@ const sketch = () => {
 
     context.fillStyle = "black";
 
-    const xAxis = width * 0.5;
-    const yAxis = height * 0.5;
-    const itemWidth = width * 0.3;
-    const itemHeight = height * 0.3;
+    const xCenterAxis = width * 0.5;
+    const yCenterAxis = height * 0.5;
+    const itemWidth = width * 0.01;
+    const itemHeight = height * 0.1;
+    let xAxis, yAxis;
 
-    context.save();
-    context.translate(xAxis, yAxis);
-    context.rotate(0.3);
+    const numOfItems = 12;
+    const radius = width * 0.3;
 
-    context.beginPath();
-    context.rect(-itemWidth * 0.5, -itemHeight * 0.5, itemWidth, itemHeight);
-    context.fill();
-    context.restore();
+    // Loop and draw item number of times
+    for (let i = 0; i < numOfItems; i++) {
+      // change the angle of rotation based on the index of the loop
+      // A circle is 360 degrees
+      const slice = degToRad(360 / numOfItems); // number of slices, converted to radians
+      const angle = slice * i; // angle of rotation
+
+      // Trigonometry
+      xAxis = xCenterAxis + radius * Math.sin(angle);
+      yAxis = yCenterAxis + radius * Math.cos(angle);
+
+      context.save();
+      context.translate(xAxis, yAxis);
+      // context.rotate(degToRad(45)); // in radians
+      context.rotate(-angle); // negative angle to rotate items
+
+      context.beginPath();
+      context.rect(-itemWidth * 0.5, -itemHeight * 0.5, itemWidth, itemHeight);
+      context.fill();
+      context.restore();
+    }
   };
 };
 
