d498bf69cfc58f1076c1b15ff8991b854ce618f3
jsguerra <jose@gcostudios.com>
2022-06-25 10:51:57 +0100

    updated file

--- sketch-01.js
+++ sketch-01.js
@@ -1,13 +1,35 @@
 const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  domension: [2000, 2000],
+  dimensions: [600, 600],
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    const itemWidth = 60;
+    const itemHeight = 60;
+    const gap = 20;
+    let xAxis, yAxis;
+
+    for (let i = 0; i < 5; i++) {
+      for (let j = 0; j < 5; j++) {
+        xAxis = 100 + (itemWidth + gap) * i;
+        yAxis = 100 + (itemHeight + gap) * j;
+
+        context.beginPath();
+        context.rect(xAxis, yAxis, itemWidth, itemHeight);
+        context.stroke();
+
+        if (Math.random() > 0.5) {
+          context.beginPath();
+          context.rect(xAxis + 8, yAxis + 8, itemWidth - 16, itemHeight - 16);
+          context.stroke();
+        }
+      }
+    }
   };
 };
 
