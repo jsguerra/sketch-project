c51533d6cbeda544c224ea6667e0270e6fd4212d
jsguerra <jose@gcostudios.com>
2022-07-02 14:10:49 +0100

    replaced functions with utility

--- sketch-02a.js
+++ sketch-02a.js
@@ -1,13 +1,19 @@
 const canvasSketch = require("canvas-sketch");
+const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
 };
 
 // Function to convert degrees into radians
-const degToRad = (degrees) => {
-  return (degrees / 180) * Math.PI;
-};
+// const degToRad = (degrees) => {
+//   return (degrees / 180) * Math.PI;
+// };
+
+// const randomRange = (min, max) => {
+//   return Math.random() * (max - min) + min;
+// };
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -29,7 +35,8 @@ const sketch = () => {
     for (let i = 0; i < numOfItems; i++) {
       // change the angle of rotation based on the index of the loop
       // A circle is 360 degrees
-      const slice = degToRad(360 / numOfItems); // number of slices, converted to radians
+      // const slice = degToRad(360 / numOfItems); // number of slices, converted to radians
+      const slice = math.degToRad(360 / numOfItems); // from utility library
       const angle = slice * i; // angle of rotation
 
       // Trigonometry
@@ -40,6 +47,8 @@ const sketch = () => {
       context.translate(xAxis, yAxis);
       // context.rotate(degToRad(45)); // in radians
       context.rotate(-angle); // negative angle to rotate items
+      // context.scale(randomRange(1, 3), 1); // custom function
+      context.scale(random.range(1, 3), 1); // from utility library
 
       context.beginPath();
       context.rect(-itemWidth * 0.5, -itemHeight * 0.5, itemWidth, itemHeight);
