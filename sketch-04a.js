ce210f41b789afa14a82244f18df440608869193
jsguerra <jose@gcostudios.com>
2022-07-23 09:03:30 +0100

    updated file

--- sketch-04a.js
+++ sketch-04a.js
@@ -1,11 +1,14 @@
 const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+const math = require("canvas-sketch-util/math");
 
 const settings = {
   dimensions: [1080, 1080],
+  animate: true
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
+  return ({ context, frame, width, height }) => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
@@ -27,17 +30,26 @@ const sketch = () => {
       // const cellXCoordinate = col * cellWidth;
       // const cellYCoordinate = row * cellHeight;
       // combined example instead of calling translate multiple times
-      const cellXCoordinate = ((col * cellWidth) + marginX + (cellWidth * 0.5));
-      const cellYCoordinate = ((row * cellHeight) + marginY + (cellHeight * 0.5));
+      const cellXCoordinate = col * cellWidth + marginX + cellWidth * 0.5;
+      const cellYCoordinate = row * cellHeight + marginY + cellHeight * 0.5;
       const cellLineWidth = cellWidth * 0.8; // 80% of cell width
       const cellLineHeight = cellHeight * 0.8; // 80% of cell height
 
+      const noise2D = random.noise2D(cellXCoordinate + frame * 20, cellYCoordinate, 0.001); // used to set the angle of rotation of the lines in the grid
+      const lineAngle = noise2D * Math.PI * 0.2; // noise2D returns -1 to 1
+      // const scale = (noise2D + 1) / 2 * 30; // to keep number positive
+      // const scale = (noise2D * 0.5 + 0.5) * 30; // same as above
+      const scale = math.mapRange(noise2D, -1, 1, 1, 30); // using utility to get the same result as the equations above
+      
+
       context.save();
       context.translate(cellXCoordinate, cellYCoordinate);
+      context.rotate(lineAngle);
       // context.translate(marginX, marginY);
       // context.translate(cellWidth * 0.5, cellHeight * 0.5);
 
-      context.lineWidth = 4;
+      // context.lineWidth = 4;
+      context.lineWidth = scale;
 
       context.beginPath();
       context.moveTo(cellLineWidth * -0.5, 0);
