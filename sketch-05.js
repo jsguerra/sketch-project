2273f902b226f523ea7784f0de3fab359e08762f
jsguerra <jose@gcostudios.com>
2022-07-24 08:54:57 +0100

    file updated

--- sketch-05.js
+++ sketch-05.js
@@ -1,13 +1,41 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [1080, 1080],
 };
+
+// Drawing a letter on Canvas
+let text = "A";
+let fontSize = "1200";
+let fontFamily = "serif";
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    context.fillStyle = 'black';
+    context.font = `${fontSize}px ${fontFamily}`;
+    context.textBaseline = "top";
+
+    const metrics = context.measureText(text);
+    const metricsXcoord = metrics.actualBoundingBoxLeft * -1;
+    const metricsYcoord = metrics.actualBoundingBoxAscent * -1;
+    const metricsWidth = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const metricsHeight = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+    const xCoord = (width - metricsWidth) * 0.5 - metricsXcoord;
+    const yCoord = (height - metricsHeight) * 0.5 - metricsYcoord;
+
+    context.save();
+    context.translate(xCoord, yCoord);
+
+    context.beginPath();
+    context.rect(metricsXcoord, metricsYcoord, metricsWidth, metricsHeight);
+    context.stroke();
+
+    context.fillText(text, 0, 0);
+    context.restore();
   };
 };
 
