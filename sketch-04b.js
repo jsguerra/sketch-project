eadd582987bddec81cc7f0eea99cdeeb560e045e
jsguerra <jose@gcostudios.com>
2022-07-24 08:28:10 +0100

    updated

--- sketch-04b.js
+++ sketch-04b.js
@@ -1,10 +1,23 @@
 const canvasSketch = require("canvas-sketch");
 const random = require("canvas-sketch-util/random");
 const math = require("canvas-sketch-util/math");
+const Tweakpane = require("tweakpane");
 
 const settings = {
   dimensions: [1080, 1080],
-  animate: true
+  animate: true,
+};
+
+const params = {
+  cols: 10,
+  rows: 10,
+  scaleMin: 1,
+  scaleMax: 30,
+  freq: 0.001,
+  amp: 0.2,
+  frame: 0,
+  animate: true,
+  lineCap: "but",
 };
 
 const sketch = () => {
@@ -12,8 +25,8 @@ const sketch = () => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    const cols = 10;
-    const rows = 10;
+    const cols = params.cols;
+    const rows = params.rows;
     const numCells = cols * rows;
 
     const gridWidth = width * 0.8; // 80% of the canvas width
@@ -35,12 +48,37 @@ const sketch = () => {
       const cellLineWidth = cellWidth * 0.8; // 80% of cell width
       const cellLineHeight = cellHeight * 0.8; // 80% of cell height
 
-      const noise2D = random.noise2D(cellXCoordinate + frame * 20, cellYCoordinate, 0.001); // used to set the angle of rotation of the lines in the grid
-      const lineAngle = noise2D * Math.PI * 0.2; // noise2D returns -1 to 1
+      const frameState = params.animate ? frame : params.frame;
+
+      // const noise2D = random.noise2D(
+      //   cellXCoordinate + frame * 20,
+      //   cellYCoordinate,
+      //   params.freq
+      // ); // used to set the angle of rotation of the lines in the grid
+      const noise3D = random.noise3D(
+        cellXCoordinate,
+        cellYCoordinate,
+        frameState * 10,
+        params.freq
+      );
+      // const lineAngle = noise2D * Math.PI * params.amp; // noise2D returns -1 to 1
+      const lineAngle = noise3D * Math.PI * params.amp; // noise2D returns -1 to 1
       // const scale = (noise2D + 1) / 2 * 30; // to keep number positive
       // const scale = (noise2D * 0.5 + 0.5) * 30; // same as above
-      const scale = math.mapRange(noise2D, -1, 1, 1, 30); // using utility to get the same result as the equations above
-      
+      // const scale = math.mapRange(
+      //   noise2D,
+      //   -1,
+      //   1,
+      //   params.scaleMin,
+      //   params.scaleMax
+      // ); // using utility to get the same result as the equations above
+      const scale = math.mapRange(
+        noise3D,
+        -1,
+        1,
+        params.scaleMin,
+        params.scaleMax
+      ); // using utility to get the same result as the equations above
 
       context.save();
       context.translate(cellXCoordinate, cellYCoordinate);
@@ -50,6 +88,7 @@ const sketch = () => {
 
       // context.lineWidth = 4;
       context.lineWidth = scale;
+      context.lineCap = params.lineCap;
 
       context.beginPath();
       context.moveTo(cellLineWidth * -0.5, 0);
@@ -61,4 +100,25 @@ const sketch = () => {
   };
 };
 
+const createPane = () => {
+  const pane = new Tweakpane.Pane();
+  let folder;
+
+  folder = pane.addFolder({ title: "Grid" });
+  folder.addInput(params, "lineCap", {
+    options: { butt: "butt", round: "round", square: "square" },
+  });
+  folder.addInput(params, "cols", { min: 2, max: 50, step: 1 });
+  folder.addInput(params, "rows", { min: 2, max: 50, step: 1 });
+  folder.addInput(params, "scaleMin", { min: 1, max: 100 });
+  folder.addInput(params, "scaleMax", { min: 1, max: 100 });
+
+  folder = pane.addFolder({ title: "Noise" });
+  folder.addInput(params, "freq", { min: -0.01, max: 0.01 });
+  folder.addInput(params, "amp", { min: 0, max: 1 });
+  folder.addInput(params, "animate");
+  folder.addInput(params, "frame", { min: 0, max: 999 });
+};
+
+createPane();
 canvasSketch(sketch, settings);
