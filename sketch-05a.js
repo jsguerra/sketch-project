456092ff090ced01ba35bbc15548fb2b4f046939
jsguerra <jose@gcostudios.com>
2022-07-24 12:01:30 +0100

    updated

--- sketch-05a.js
+++ sketch-05a.js
@@ -4,6 +4,9 @@ const settings = {
   dimensions: [1080, 1080],
 };
 
+// Canvas sketch is asyncronous so manager allows for trigger re-rendering
+let manager;
+
 // Drawing a letter on Canvas
 let text = "A";
 let fontSize = "1200";
@@ -14,15 +17,17 @@ const sketch = () => {
     context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
-    context.fillStyle = 'black';
+    context.fillStyle = "black";
     context.font = `${fontSize}px ${fontFamily}`;
     context.textBaseline = "top";
 
     const metrics = context.measureText(text);
     const metricsXcoord = metrics.actualBoundingBoxLeft * -1;
     const metricsYcoord = metrics.actualBoundingBoxAscent * -1;
-    const metricsWidth = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
-    const metricsHeight = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+    const metricsWidth =
+      metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+    const metricsHeight =
+      metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 
     const xCoord = (width - metricsWidth) * 0.5 - metricsXcoord;
     const yCoord = (height - metricsHeight) * 0.5 - metricsYcoord;
@@ -39,4 +44,48 @@ const sketch = () => {
   };
 };
 
-canvasSketch(sketch, settings);
+// We listen to our events, assign a new letter and render a new frame
+const onKeyUp = (e) => {
+  text = e.key.toUpperCase();
+  manager.render();
+};
+
+// Add an event listener to document to listen to key strokes
+document.addEventListener("keyup", onKeyUp);
+
+// We wrap canvasSketch in an async function to await our events
+const start = async () => {
+  manager = await canvasSketch(sketch, settings);
+};
+
+// The function must be executed to work
+start();
+
+/*
+const url = 'https://picsum.photos/200';
+
+const loadMeSomeImage = (url) => {
+	return new Promise((resolve, reject) => {
+		const img = new Image();
+		img.onload = () => resolve(img);
+		img.onerror = () => reject();
+		img.src = url;
+	});
+};
+
+const start = async () => {
+	const img = await loadMeSomeImage(url);
+	console.log('image width', img.width);
+	console.log('this line');
+};
+
+// const start = () => {
+// 	loadMeSomeImage(url).then(img => {
+// 		console.log('image width', img.width);
+// 	});
+// 	console.log('this line');
+// };
+
+
+start();
+*/
